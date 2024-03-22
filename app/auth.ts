7797805b7ee6302718c5b66b0c6a5860a751ce08
Andrew Jensen <acjensen@Andrews-MacBook-Pro.local>
2024-03-21 21:19:19 -0500

    Partially implement auth

---
+++ app/auth.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from "bcrypt-ts";
+import { getUser } from "./db";
+import { authConfig } from "./auth.config";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize({ email, password }: any) {
+        let user = await getUser(email);
+        if (user.length === 0) return null;
+        // let passwordsMatch = await compare(password, user[0].password!);
+        const passwordsMatch = password === user[0].password!;
+        if (passwordsMatch) return user[0] as any;
+      },
+    }),
+  ],
+});
---
+++ app/db.ts
@@ -0,0 +1,13 @@
+export const getUser = async (email: string) => {
+  return [
+    {
+      id: "uniqueId",
+      email: "asdf@asdf.com",
+      password: "asdfpwd",
+    },
+  ];
+};
+
+export const createUser = async (email: string, password: string) => {
+  //
+};
