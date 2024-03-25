b6b42ed7b204dcffe72676353334825995d5f48a
Andrew Jensen <andrew.charles.jensen@gmail.com>
2024-03-25 01:40:50 -0500

    Fix logout route

--- app/logout/page.tsx
+++
@@ -1,7 +0,0 @@
-import { GET } from "./route";
-
-export async function Page(props: any) {
-  // const router = useRouter();
-  GET();
-  // router.back();
-}
--- app/logout/route.ts
+++ app/logout/route.ts
@@ -3,5 +3,5 @@ import { signOut } from "../auth/auth";
 
 export async function GET() {
   await signOut();
-  return NextResponse.json({ message: "You've been signed out." });
+  return new NextResponse("You've been signed out.");
 }
