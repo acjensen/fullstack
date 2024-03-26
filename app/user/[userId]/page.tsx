a72bdc3071996df8751fe0313bc74d608a510672
Andrew Jensen <andrew.charles.jensen@gmail.com>
2024-03-25 23:29:48 -0500

    Basic user page

---
+++ app/user/[userId]/page.tsx
@@ -0,0 +1,12 @@
+import { get } from "../../server/actions";
+
+export default async function Page({ params }: { params: { userId: string } }) {
+  const { userId } = params;
+  const user = await get(decodeURIComponent(userId));
+  return (
+    <div>
+      <h1>User ID: {JSON.stringify(params)}</h1>
+      <pre>User record: {JSON.stringify(user, null, 2)}</pre>
+    </div>
+  );
+}
